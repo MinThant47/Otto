01fbe70a8ac82482884d43ccd8efc6c4f9a89024
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2024-12-02 20:38:26 +0630

    Update firebase_func.py

--- firebase_func.py
+++ firebase_func.py
@@ -8,10 +8,14 @@ json_data = st.secrets["FIREBASE_CRED"]
 firebase_credentials = json.loads(json_data)
 cred = credentials.Certificate(firebase_credentials)
 
-# cred = credentials.Certificate("https://github.com/MinThant47/Otto/blob/main/otto-8275b-firebase-adminsdk-1zc00-881e8332a3.json")
-firebase_admin.initialize_app(cred, {
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://otto-8275b-default-rtdb.asia-southeast1.firebasedatabase.app/'
 })
+# cred = credentials.Certificate("https://github.com/MinThant47/Otto/blob/main/otto-8275b-firebase-adminsdk-1zc00-881e8332a3.json")
+# firebase_admin.initialize_app(cred, {
+#     'databaseURL': 'https://otto-8275b-default-rtdb.asia-southeast1.firebasedatabase.app/'
+# })
 
 # Reference to the database
 ref = db.reference('/')
