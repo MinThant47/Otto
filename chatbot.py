0b447d03c6abe516c9931207818974129de8c9bd
MinThant47 <63152338+MinThant47@users.noreply.github.com>
2024-12-02 21:30:04 +0630

    Used natural groq

--- chatbot.py
+++ chatbot.py
@@ -1,34 +1,27 @@
-from aisuite.client import AISuite
 import streamlit as st
-# import os
-# from dotenv import load_dotenv
-# load_dotenv()
-# os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
-groq_api_key = st.secrets["GROQ_API_KEY"]
-client = AISuite(api_key=groq_api_key)
+from groq import Groq
 
-# messages = [
-#     {"role": "system", "content": "You are a helpful agent, who answers with brevity."},
-#     {"role": "user", "content": 'Hi'},
-# ]
+groq_api_key = st.secrets["GROQ_API_KEY"]
 
 # # Request a response from the model
 # response = client.chat.completions.create(model="groq:llama-3.2-3b-preview", messages=messages)
 
 def ask_question(message, sys_message="You are a helpful agent.",
          model="groq:llama-3.2-3b-preview"):
+
+         client = Groq(
+             api_key=groq_api_key,
+         )
          
-
-    client = ai.Client()
-    
-    messages = [
-        {"role": "system", "content": sys_message},
-        {"role": "user", "content": "Please give a one line answer to this question:" + message }
-    ]
-
-    # Send the messages to the model and get the response
-    response = client.chat.completions.create(model=model, messages=messages)
+         chat_completion = client.chat.completions.create(
+                messages = [
+                          {"role": "system", "content": sys_message},
+                          {"role": "user", "content": "Please give a one line answer to this question:" + message }
+                      ],
+             model=model,
+         )
+         response = chat_completion.choices[0].message.content
 
     # Return the content of the model's response
-    return response.choices[0].message.content
+    return response
 
